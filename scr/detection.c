3a2425eb5fa49070054faf3137f8f6b3c1c9c79e
Pedro Soares <P.Soares1997@live.com.pt>
2020-08-12 20:47:45 +0100

    Changed functions arguments type from uint to double, removed arrays declaration in the header file, and added sample_array and power_array has function arguments

--- scr/detection.c
+++ scr/detection.c
@@ -1,15 +1,15 @@
-#include "detection.h"
+#include <detection.h>
 
-#include "user_config.h"
+#include <user_config.h>
 
 #include <stdint.h>
+#include <stddef.h>
 #include <math.h>
-#include <c_types.h>
 
 
 uint16_t adc_num = num_samples;
 
-double mean(uint64_t arr[]){
+double mean(double arr[]){
   double total = 0;
   for(uint8_t i = 0; i < num_samples; i++){
     total += arr[i];
@@ -17,31 +17,31 @@ double mean(uint64_t arr[]){
   return total / num_samples;
 }
 
-double std(uint64_t arr[], double media){
+double std(double arr[], double media){
   double std = 0;
   for(int i = 0; i < num_samples; i++){
     std = std + (arr[i] * arr[i]);
   }
   double tmp = std / (double) adc_num;
-  return sqrt(adc_num);
+  return sqrt(tmp);
 }
 
-void powerSequence(uint16_t arr[]){
+int powerSequence(double * samples, double * power){
   double sum = 0;
   for(uint8_t i = 0; i < num_samples; i++){
     sum = sum + (arr[i] * arr[i] ) * (i + k * num_samples);
   }
   sum = 1 / num_samples * sum;
-  power_arr[k] = sum;
-  return;
+  power[k] = sum;
+  k++
+  return k;
 }
 
-double threshold(uint16_t sampleArr[]){
-    powerSequence(sample_arr);
+double threshold(double * power){
 
-    double mean_tmp = mean(power_arr);
+    double mean_tmp = mean(power);
 
-    double std_tmp = std(power_arr, mean_tmp);
+    double std_tmp = std(power, mean_tmp);
 
     double th = 1 * std_tmp + mean_tmp;
     return th;
